fd88e7ab18928bebcdcca00e75313f75adc403c2
GIRI-R-S <giri.srinivasa.r@gmail.com>
2025-06-27 21:00:22 +0530

    Update Classes.java

--- Week_1/Design patterns and Priciples/Factory Method/Input/Classes.java
+++ Week_1/Design patterns and Priciples/Factory Method/Input/Classes.java
@@ -1,1 +1,44 @@
+package FactoryMethod;
+interface Document {
+    void open();
+}
 
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document...");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document...");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document...");
+    }
+}
+
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
