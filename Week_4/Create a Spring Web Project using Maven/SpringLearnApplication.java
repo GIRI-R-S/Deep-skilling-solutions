b1388ea5865a829b90948ba9c5973662c2c7bfd8
GIRI-R-S <giri.srinivasa.r@gmail.com>
2025-07-14 16:39:26 +0530

    Update SpringLearnApplication.java

--- Week_4/Create a Spring Web Project using Maven/SpringLearnApplication.java
+++ Week_4/Create a Spring Web Project using Maven/SpringLearnApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringLearnApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
-        System.out.println("Spring Boot App Started!");
+        System.out.println("Spring Boot Learner App Started!");
     }
 }
 
