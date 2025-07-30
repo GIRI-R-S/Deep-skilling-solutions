d58893af63a90a9ca9254ba0df484a1c4f3d7b6b
GIRI R S <giri.srinivasa.r@gmail.com>
2025-07-31 01:15:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Welcome the first session of React.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
